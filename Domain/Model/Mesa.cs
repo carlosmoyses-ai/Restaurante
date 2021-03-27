f72bb6e5dd74d6d454aa2dbf9d7a49701834709a
Carlos Moyses <carlosmoyses@gmail.com>
2021-03-27 01:53:09 -0300

    Implementando uma Lista Itens em Mesa

--- Domain/Model/Mesa.cs
+++ Domain/Model/Mesa.cs
@@ -1,9 +1,23 @@
 ﻿using System;
-
+using Domain.Model;
 namespace Model.Domain
 {
     public class Mesa
     {
+        const int MAXQUANT = 10;
+        private int quantidadeItensVendidos = 0;
         public int Numero { get; private set; }
+        public Item[] Itens { get;}  = new Item[MAXQUANT];
+
+        public Mesa(int numero)
+        {
+            Numero = numero;
+        }
+
+        public void RegistrarItem(Item item)
+        {
+            if (quantidadeItensVendidos < MAXQUANT)
+                Itens[quantidadeItensVendidos++] = item;
+        }
     }
 }
